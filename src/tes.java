6ae1057d69cdd87bd71355650ed372ef58b805a5
Satelllite <Satelllite@172.16.15.48>
2015-05-05 11:21:31 +0700

    tugas tb minggu 2

---
+++ src/tes.java
@@ -0,0 +1,17 @@
+
+
+public class tes {
+	public static void main(String[] args) {       
+	    ruangkelas in = new ruangkelas();
+	    in.input_identitas_ruang_kelas();
+	   // in.input_kondisi_ruang_kelas();
+	    in.kondisi_dan_posisi_sarana();
+	    //in.input_lingkungan_ruang_kelas();
+	    //in.kebersihan_ruang_kelas();
+	   // in.kenyamanan_ruang_kelas();
+	   // in.keamanan_ruang();
+            in.tampil();
+	}  
+	    
+	
+	}
