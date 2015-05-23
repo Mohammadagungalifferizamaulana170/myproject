41dff038ca269e0f69e5ed62f7bbaf8fddb54ab2
TOSHIBA <TOSHIBA@Master>
2015-05-23 17:20:50 +0700

    rachda

--- src/IO.java
+++ src/IO.java
@@ -100,7 +100,7 @@ public class IO extends supersuperclass implements out {
       a = new setget(ini.getJumlah_steker(),ini.getKondisi_steker(),ini.getPosisi_steker(),ini.getJumlah_kabel_LCD(),
               ini.getKondisi_kabel_LCD(),ini.getPosisi_kabel_LCD(),ini.getJumlah_lampu(),ini.getKondisi_lampu(),
               ini.getPosisi_lampu(),ini.getJumlah_kipas_angin(),ini.getKondisi_kipas_angin(),ini.getPosisi_kipas_angin(),ini.getJumlah_AC(),ini.getKondisi_AC(),ini.getPosisi_AC(),ini.getSSID(),
-              ini.getBandwidth(),ini.getJumlah_CCTV(),ini.getKondisi_CCTV(),ini.getPosisi_CCTV());
+              ini.getBandwidth(),ini.getJumlah_CCTV(),ini.getKondisi_CCTV(),ini.getPosisi_CCTV());//
     }
     
     @Override
